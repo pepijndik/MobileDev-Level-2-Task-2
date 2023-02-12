c186c25b4f0a27e604651ac41cc9d48caf0b6e4b
Pepijn dik <pepijn@pdik.nl>
2023-02-12 21:06:39 +0100

    Level 2 Task 2

--- app/src/main/java/nl/pdik/level2/task2/MainActivity.kt
+++ app/src/main/java/nl/pdik/level2/task2/MainActivity.kt
@@ -1,17 +1,27 @@
 package nl.pdik.level2.task2
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nl.pdik.level2.task2.model.Equation
 import nl.pdik.level2.task2.ui.theme.Task2Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ScreenContent()
+                    Task2()
                 }
             }
         }
@@ -31,23 +41,143 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Task(){
+fun Task2(){
+    val context = LocalContext.current
+    val equation = remember { mutableStateOf(Equation("-", "-", "?")) }
     Scaffold(  topBar = {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.app_name)) }
             )
         },
-        content = {padding -> ScreenContent(Modifier.padding(padding))  }
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { stringResource(id = R.string.next) },
+                onClick = {  equation.value = generateEquation()})
+        }
+        ,
+        content = {padding -> ScreenContent(Modifier.padding(padding),context = context, equation = equation)  }
+    )
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier, context: Context, equation: MutableState<Equation>){
+    Column(modifier) {
+        ScreenTitle()
+        Column() {
+            EquationHeaders()
+            AnswerButtons(
+                checkEquation = {
+                        check: Boolean ->
+                    if(check && equation.value.firstArg == "T" && equation.value.secondArg == "T" && equation.value.anwser =="T"){
+                        equation.value = Equation("✅","✅","✅")
+                        informUser(context = context, R.string.correct)
+                    }else{
+                        informUser(context = context, R.string.incorrect)
+                    }
+                }
+            )
+            if(!equation.value.anwser.contains("?",) || !equation.value.anwser.contains("✅")){
+                EquationValues(equation.value)
+            }
+        }
+
+    }
 }
 @Composable
 fun ScreenTitle() {
-    Text(text = "Hello $name!")
+    Row(
+    horizontalArrangement = Arrangement.Center
+    )
+    {
+        Text(
+            text = stringResource(id = R.string.title),
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            style = MaterialTheme.typography.h4
+        )
+    }
+
+}
+@Composable
+fun EquationHeaders(){
+    Row(
+         modifier = Modifier.fillMaxWidth(),
+         Arrangement.SpaceAround,
+    ) {
+        Text(text = stringResource(id = R.string.first),fontWeight = FontWeight.Bold,)
+        Text(text = stringResource(id = R.string.second),fontWeight = FontWeight.Bold,)
+        Text(text = stringResource(id = R.string.and), fontWeight = FontWeight.Bold,)
+    }
+
+}
+
+@Composable
+fun EquationValues(equation: Equation){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        Arrangement.SpaceAround,
+    ) {
+        Text(text = equation.firstArg,fontWeight = FontWeight.Bold,)
+        Text(text = equation.secondArg,fontWeight = FontWeight.Bold,)
+        Text(text = equation.anwser, fontWeight = FontWeight.Bold,)
+    }
+}
+private fun informUser(context: Context, msgId: Int) {
+    Toast.makeText(context, context.getString(msgId), Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * Create Random Equation
+ */
+private fun generateEquation(): Equation{
+    return Equation(generateRandomConjunctionString(), generateRandomConjunctionString(), generateRandomConjunctionString())
+}
+
+/**
+ * T | F string randomizer
+ */
+private fun generateRandomConjunctionString(): String {
+    val trueString = "T"
+    val falseString = "F"
+    return  if ((0..1).random() == 0) trueString else falseString
+}
+
+@Composable
+private fun AnswerButtons(checkEquation: (Boolean) -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Top
+    ){
+        Button(
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xff4caf50)),
+            onClick = {
+                checkEquation(true)
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.width(100.dp),
+        ) {
+            Text(text = stringResource(id = R.string.btn_true))
+        }
+        Button(
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+            onClick = {
+                checkEquation(false)
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.width(100.dp),
+        ) {
+            Text(text = stringResource(id = R.string.btn_false))
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Task2Theme {
-        ScreenContent()
+        Task2()
     }
 }
---
+++ app/src/main/java/nl/pdik/level2/task2/model/Equation.kt
@@ -0,0 +1,3 @@
+package nl.pdik.level2.task2.model
+
+data class Equation(val firstArg: String,val secondArg: String, val anwser: String )
